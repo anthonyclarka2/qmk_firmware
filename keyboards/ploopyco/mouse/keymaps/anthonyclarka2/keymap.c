0791ef226e7dce8f49b1f361e790c82e50ee9573
Anthony Jarvis-Clark <anthonyclarka2@gmail.com>
2025-10-29 10:54:20 +0000

    feat: modify 2nd layer side buttons

--- keyboards/ploopyco/mouse/keymaps/anthonyclarka2/keymap.c
+++ keyboards/ploopyco/mouse/keymaps/anthonyclarka2/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(/* Base */
                  C(KC_UP), KC_BTN1, KC_BTN3, KC_BTN2, MO(1), KC_PGDN, KC_PGUP, DPI_CONFIG),
     [1] = LAYOUT(/* Extras */
-                 RGB_HUI, C(KC_V), RGB_TOG, S(A(G(KC_V))), MO(1), KC_VOLD, KC_VOLU, RESET)
+                 RGB_HUI, C(KC_V), RGB_TOG, S(A(G(KC_V))), MO(1), KC_HOME, KC_END, RESET)
 }; // KC_LSFT(KC_LOPT(KC_LCMD(KC_V)))
 
 void eeconkfig_init_user(void) {
